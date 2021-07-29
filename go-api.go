bbef850682ccab7685a5f2063a2b769301ce6e00
Trevor Jones <trevorryanjones@gmail.com>
2021-07-29 02:21:51 -0400

    cleaned up code a bit

--- go-api.go
+++ go-api.go
@@ -5,16 +5,57 @@ import (
 	"net/http"
 )
 
-type server struct{}
+//type server struct{}
+//
+//func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+//	w.Header().Set("Content-Type", "application/json")
+//	switch r.Method {
+//	case "GET":
+//		w.WriteHeader(http.StatusOK)
+//		w.Write([]byte(`{"message": "get called"}`))
+//	case "POST":
+//		w.WriteHeader(http.StatusOK)
+//		w.Write([]byte(`{"message": "post called"}`))
+//	case "PUT":
+//		w.WriteHeader(http.StatusOK)
+//		w.Write([]byte(`{"message": "put called"}`))
+//	case "DELETE":
+//		w.WriteHeader(http.StatusOK)
+//		w.Write([]byte(`{"message": "delete called"}`))
+//	default:
+//		w.WriteHeader(http.StatusOK)
+//		w.Write([]byte(`{"message": "not found"}`))
+//	}
+//}
+//
+//func main() {
+//	s := &server{}
+//	http.Handle("/", s)
+//	log.Fatal(http.ListenAndServe(":8080", nil))
+//}
 
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "hello world"}`))
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "get called"}`))
+	case "POST":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "post called"}`))
+	case "PUT":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "put called"}`))
+	case "DELETE":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "delete called"}`))
+	default:
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "not found"}`))
+	}
 }
 
 func main() {
-	s := &server{}
-	http.Handle("/", s)
+	http.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
